4704611d538231c722f79f19d6a76d271de9c467
skyhoffert <skwylarh@yahoo.com>
2018-05-09 18:53:47 -0400

    Starting to develop transferring data across channel

--- lib/channel.py
+++ lib/channel.py
@@ -5,5 +5,13 @@
 # Modified By:   Sky Hoffert
 
 
+import math
+
+
 class Channel():
     '''Channel that packets are sent across'''
+
+    def __init__(self, frequency, distance, port=8200):
+        val = 4 * 3.1415926 * distance * frequency / 299792458
+        raw = pow(val, 2)
+        self._loss = 10 * math.log10(raw)
